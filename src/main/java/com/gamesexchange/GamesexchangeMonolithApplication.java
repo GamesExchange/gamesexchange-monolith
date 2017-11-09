ea425322a2da50ebee6038d1e9b6f4f10614ee72
Seophyn <Jimmygustafsson93@gmail.com>
2017-11-09 20:09:21 +0100

    Initial commit

---
+++ src/main/java/com/gamesexchange/GamesexchangeMonolithApplication.java
@@ -0,0 +1,12 @@
+package com.gamesexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesexchangeMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesexchangeMonolithApplication.class, args);
+	}
+}
